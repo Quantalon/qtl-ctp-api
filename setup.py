20e298cc9f54bcb111f3ac7b2d8b64dd6bc894e0
G_will <g_will@ieqi.com>
2022-12-09 09:09:49 +0800

    update

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ from setuptools import find_packages
 setup(
     name="qtl-ctp-api",
     version="6.6.9",
+    long_description="QTL CTP Api",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     cmake_install_dir="src/qtl_ctp_api",
